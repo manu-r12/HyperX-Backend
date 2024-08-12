7b25a695c03aacdefb1995cd81169fe0487b14d3
Manu R <manurajbhar12@gmail.com>
2024-08-12 18:10:05 +0530

    adding initial files of the project

---
+++ src/test/java/tech/manu/hyperx/ai/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.manu.hyperx.ai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
